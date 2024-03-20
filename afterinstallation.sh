366219a781de921f98e96cac3dcccebbeecf9624
musha05 <132899324+musha05@users.noreply.github.com>
2024-03-20 12:39:48 +0530

    Delete afterinstallation.sh

--- afterinstallation.sh
+++
@@ -1,4 +0,0 @@
-EC2_INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
-sed -i "s|application was deployed on|application was deployed on $EC2_INSTANCE_ID in $AZ|" /var/www/html/index.html
-chmod 664 /var/www/html/index.html
