2be4c38eb18e5e2498907dd75a67f0c636007f39
musha05 <132899324+musha05@users.noreply.github.com>
2024-03-20 12:39:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+const express = require('express');
+const app = express();
+const hostname = '13.233.149.112'; // Your server ip address
+const port = 3000;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          This is sample application for aws devops
-        
-        </a>
-        <h1 style={{color:'pink'}}>Welcome to CI/CD testing pipeline by using aws services</h1>
-        <h2 style={{color:'yellow'}}>This is sample testing application</h2>
-      </header>
-    </div>
-  );
-}
+const version = '3,000,000';
 
-export default App;
+app.get('/', (req, res) => {
+    // set response content    
+        res.send(`<html>
+                    <body>
+                        <h1 style="color:blue;text-align: center;margin-top: 100px;"> [Version ${version}]: THis is AMAZING!!! Like & Subscribe!</h1>
+                        <div style="position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%)">
+                            <img src="https://picsum.photos/400/400?random=1">
+                        </div>
+                    </body>
+                   </html>`);
+ 
+  console.log(`[Version ${version}]: New request => http://${hostname}:${port}`+req.url);
+
+})
+
+app.listen(port, () => {
+    console.log(`[Version ${version}]: Server running at http://${hostname}:${port}/`);
+})
