6f704f10c60096f72226b50e498b0c2551ccb3f4
musha05 <132899324+musha05@users.noreply.github.com>
2024-03-21 00:07:04 +0530

    Update application-stop.sh

--- scripts/application-stop.sh
+++ scripts/application-stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Stop any running server process
-sudo systemctl stop apache2   # Replace with your server's stop command if using a different server
+sudo systemctl stop httpd   # Replace with your server's stop command if using a different server
